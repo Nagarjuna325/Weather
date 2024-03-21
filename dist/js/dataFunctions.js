193cf1799b76db5bcc221ba7ea87d760164216a8
Nagarjuna325 <nagarjunreddybolla@gmail.com>
2024-03-20 19:44:22 -0500

    commit

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -41,23 +41,23 @@ export const setLocationObject = (locationObj, coordsObj) => {
       return weatherJson;
     } catch (err) {
       console.error(err);
-      return null;
+      // return null;
     }
   };
   
   export const getCoordsFromApi = async (entryText, units) => {
-    const regex = /^\d+$/g;
-    const flag = regex.test(entryText) ? "zip" : "q";
-    const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
-    const encodedUrl = encodeURI(url);
-    try {
-      const dataStream = await fetch(encodedUrl);
-      const jsonData = await dataStream.json();
-      console.log(jsonData);
-      return jsonData;
-    } catch (err) {
-      console.error(err.stack);
-    } 
+    // const regex = /^\d+$/g;
+    // const flag = regex.test(entryText) ? "zip" : "q";
+    // const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+    // const encodedUrl = encodeURI(url);
+    // try {
+    //   const dataStream = await fetch(encodedUrl);
+    //   const jsonData = await dataStream.json();
+    //   console.log(jsonData);
+    //   return jsonData;
+    // } catch (err) {
+    //   console.error(err.stack);
+    // } 
   
     const urlDataObj = {
       text: entryText,
